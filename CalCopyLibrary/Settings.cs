89b5198ae072c2195c2f181d8e1804aff3e869d8
Steven Heigl <sheigl@stevenheiglsmbp.attlocal.net>
2017-07-08 10:40:04 -0400

    added old calcopy lib

---
+++ CalCopyLibrary/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CalCopyLibrary
+{
+	public class Settings
+	{		
+		public string MasterTemplateFile { get; set; }
+		public string ProofingFolder { get; set; }
+		public string WorkingCalendarFolder { get; set; }
+		public string APIKey { get; set; }
+		public string APIUrl { get; set; }
+		public string SecretKey { get; set; }
+		public string ProoferInitials { get; set; }
+		public DateTime? CreatedDate { get; set; }
+		public DateTime? LastModifiedDate { get; set; }
+	}
+}
